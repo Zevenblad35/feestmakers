9e2c7682843bfd5ea0d6c8cc24e238d601541c9b
Zevenblad35 <peterrutgers@live.nl>
2026-06-02 11:09:16 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://defeestmakers.nl',
+  output: 'static',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
